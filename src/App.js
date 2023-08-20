6fba3b38b1ebd5288bd3da56cf9f4999529b218e
mohit24raut <128046366+mohit24raut@users.noreply.github.com>
2023-08-20 21:55:56 +0530

    adas

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import RetailerHomePage from './Components/RetailerHomePage';
+import RetailerRegistration from './Components/RetailerRegistration';
+import { AddProductForm } from './Components/AddProductForm';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <AddProductForm/>
+    </>
   );
 }
 
